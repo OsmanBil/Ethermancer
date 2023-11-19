bb9727645c91e02edabddd5d2794bd0379fc6868
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-11-19 21:58:18 +0100

    update

--- ethermancer-dashboard/ethermancer-dashboard-backend/bin/deploy.sh
+++ ethermancer-dashboard/ethermancer-dashboard-backend/bin/deploy.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Initialize the Elastic Beanstalk environment 
-eb init "testumgebung" -p "Node.js 18" -r us-east-1
-
-# Set environment variables
-eb setenv POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USER=$POSTGRES_USER
+eb init "AWS-Testumgebung" -p "Node.js 18" -r us-east-1
 
 # Create and deploy a new application release
-eb deploy "Testumgebung-env"
+eb deploy "AWS-Testumgebung-env"
